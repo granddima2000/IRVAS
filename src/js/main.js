e7897f3665d976ffc36084f3fd96f52adac165b2
Dima <dima.rastorguev.2016@mail.ru>
2024-03-17 22:18:42 +0300

    add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import "./slider";
 import modals from './modules/modals'
+import tabs from "./modules/tabs";
 
 window.addEventListener('DOMContentLoaded', () => { // Отвечает за то, что наши скрипты начинают выполняться только тогда, когда DOM структура на нашей страницы вообще готова
     modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
 });
